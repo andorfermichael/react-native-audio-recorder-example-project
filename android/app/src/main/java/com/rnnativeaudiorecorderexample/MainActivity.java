67a5edec4a3d30ad58cb2399bde6f94b6d6bc3bb
Michael Andorfer <office@michael-andorfer.com>
2018-09-24 13:33:55 +0200

    integrate android version of audio-recorder

--- android/app/src/main/java/com/rnnativeaudiorecorderexample/MainActivity.java
+++ android/app/src/main/java/com/rnnativeaudiorecorderexample/MainActivity.java
@@ -1,5 +1,10 @@
 package com.rnnativeaudiorecorderexample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -11,5 +16,34 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "RNNativeAudioRecorderExample";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+      // Request microphone permission
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+              != PackageManager.PERMISSION_GRANTED) {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                123);
+      }
+
+      // Request permission for writing external storage
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+              != PackageManager.PERMISSION_GRANTED) {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                123);
+      }
+
+      // Request permission for reading external storage
+      if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+              != PackageManager.PERMISSION_GRANTED) {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                123);
+      }
     }
 }
