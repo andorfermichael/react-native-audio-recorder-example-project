8d17e735168bc4c3124c54520b16d85f3fe3f1a8
Michael Andorfer <office@michael-andorfer.com>
2018-11-12 13:26:20 +0100

    feat: implement player

--- App.js
+++ App.js
@@ -10,6 +10,10 @@ import React from 'react';
 import type { ElementRef } from 'react';
 import {Button, Dimensions, StyleSheet, View, ScrollView} from 'react-native';
 
+import DummyWaveLine from './DummyWaveLine';
+
+import Video from 'react-native-video';
+
 import { AudioRecorder, AudioPlayerPlot } from 'react-native-native-audio-recorder';
 
 
@@ -20,13 +24,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  scruber: {
-    position: 'absolute',
-    left: Dimensions.get('window').width / 2,
-    backgroundColor: 'black',
-    width: 5,
-    height: '100%'
   }
 });
 
@@ -37,8 +34,11 @@ export default class App extends React.Component<Props> {
 
   scrollViewRef: ?ElementRef<ScrollView> = null;
 
+  playerRef: ?ElementRef<Video> = null;
+
   state = {
     track: null,
+    isRecording: false,
     isPlaying: false
   };
 
@@ -60,16 +60,35 @@ export default class App extends React.Component<Props> {
     this.scrollViewRef = ref;
   };
 
-  handleStartRecording = () => {
+  setPlayerRef = (playerRef: ?ElementRef<Video>) => {
+    this.playerRef = playerRef;
+  };
+
+  handleTrackLoad = () => {
+    this.setState({isPlaying: true});
+
+    this.playerRef.seek(0);
+  };
+
+  handlePlaybackEnd = () => {
     this.setState({isPlaying: false});
 
+    this.playerRef.seek(0);
+
+    this.forceUpdate();
+  };
+
+
+  handleStartRecording = () => {
     if (this.audioRecorderRef) {
+      this.setState({isRecording: true});
       this.audioRecorderRef.startRecording();
     }
   };
 
   handleStopRecording = () => {
     if (this.audioRecorderRef) {
+      this.setState({isRecording: false});
       this.audioRecorderRef.stopRecording()
         .then(params => {
           const {
@@ -82,16 +101,22 @@ export default class App extends React.Component<Props> {
           };
 
           this.setState({track: currentTrack});
-          this.setState({isPlaying: true});
+        })
+        .then(params => {
+          // TODO: This line makes leads to app crash because it executes the rendering of the file waveform
+          // TODO: However the file is not ready at this point of time
+          // this.audioPlayerPlotRef.renderByFile(this.state.track.url);
+          this.forceUpdate();
         });
     }
   };
 
   handlePlayRecording = () => {
-    if (this.audioPlayerPlotRef) {
-      this.audioPlayerPlotRef.renderByFile(this.state.track.url);
-      this.forceUpdate();
-    }
+    const playing = !this.state.isPlaying;
+
+    this.setState({isPlaying: playing});
+
+    this.forceUpdate();
   };
 
   render() {
@@ -103,24 +128,34 @@ export default class App extends React.Component<Props> {
 
     if (this.state.track != null) {
       pixelsPerSecond = windowWidth / 6;
-      plotWidth = (parseInt(this.state.track) / 1000) * pixelsPerSecond + windowWidth;
+      plotWidth = (parseInt(this.state.track.duration) / 1000) * pixelsPerSecond + windowWidth;
     }
-
-    console.log(this.state.isPlaying);
 
     return (
       <View style={styles.container}>
+        <DummyWaveLine width={windowWidth} height={plotHeight} />
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           ref={this.setScrollViewRef}
         >
-          {!!this.state.isPlaying && (
+          {!!this.state.track && (
             <AudioPlayerPlot
               width={plotWidth}
               height={plotHeight}
               pixelsPerSecond={pixelsPerSecond}
               ref={this.setAudioPlayerPlotRef}
+            />
+          )}
+
+          {this.state.isPlaying && (
+            <Video
+              audioOnly
+              paused={!this.state.isPlaying}
+              source={{ uri: this.state.track.url }}
+              ref={this.setPlayerRef}
+              onLoad={this.handleTrackLoad}
+              onEnd={this.handlePlaybackEnd}
             />
           )}
         </ScrollView>
@@ -131,9 +166,9 @@ export default class App extends React.Component<Props> {
           ref={this.setAudioRecorderRef}
         />
 
-        <Button title="Start Recording" onPress={this.handleStartRecording} />
-        <Button title="Stop Recording" onPress={this.handleStopRecording} />
-        <Button title="Play Recording" onPress={this.handlePlayRecording} />
+        <Button title="Start Recording" onPress={this.handleStartRecording} disabled={this.state.isRecording} />
+        <Button title="Stop Recording" onPress={this.handleStopRecording} disabled={!this.state.isRecording} />
+        <Button title="Play Recording" onPress={this.handlePlayRecording} disabled={this.state.isRecording || !this.state.track}/>
       </View>
     );
   }
